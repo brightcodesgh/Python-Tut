53f53e863f7a68879464d85dc7468e8469e1925e
DArkwa Bright <darkwadev@gmail.com>
2026-01-26 21:41:21 +0000

    Int and string DAtatypes added

---
+++ dataTypes.py
@@ -0,0 +1,12 @@
+#Number - Getting the datatype of a value, variable or an object 
+a,b,c,d = "name", 12, 30.5, False
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+#String - assigning a multiline string
+my_Statement = """This is my very little state., 
+I want to say Python is also cool, 
+i never liked it though"""
+print(my_Statement) 
