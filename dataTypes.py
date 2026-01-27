437b84ee234c9d389b092e348e2b9a072c1d02f8
DArkwa Bright <darkwadev@gmail.com>
2026-01-27 14:10:37 +0000

    dataTypes modified to only contain the data types brief presentation

--- dataTypes.py
+++ dataTypes.py
@@ -1,24 +1,8 @@
 #Number - Getting the datatype of a value, variable or an object 
-a,b,c,d = "name", 12, 30.5, False
+a,b,c,d, e = "name", 12, 30.5, False, 2 + 3j
 print(type(a))
 print(type(b))
 print(type(c))
 print(type(d))
+print(type(e))
 print(" ")
-
-#String - assigning a multiline string
-my_Statement = """This is my very little state., 
-I want to say Python is also cool, 
-i never liked it though"""
-print(my_Statement) 
-print(" ")
-
-#Working with strings as arrays
-    #getting a character from a string
-h = "This is a subtle comment"
-print(h[1])
-print(" ")
-
-#Looping through a string
-for x in "Banana":
-    print(x)
