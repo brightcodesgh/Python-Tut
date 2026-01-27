0bd7792f5e2577fa5e76eafcc2346933375997b9
DArkwa Bright <darkwadev@gmail.com>
2026-01-27 13:44:58 +0000

    looping through string syntax added

--- dataTypes.py
+++ dataTypes.py
@@ -4,9 +4,21 @@ print(type(a))
 print(type(b))
 print(type(c))
 print(type(d))
+print(" ")
 
 #String - assigning a multiline string
 my_Statement = """This is my very little state., 
 I want to say Python is also cool, 
 i never liked it though"""
 print(my_Statement) 
+print(" ")
+
+#Working with strings as arrays
+    #getting a character from a string
+h = "This is a subtle comment"
+print(h[1])
+print(" ")
+
+#Looping through a string
+for x in "Banana":
+    print(x)
