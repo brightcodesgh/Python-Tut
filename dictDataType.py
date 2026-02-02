8aef2ef4a1b3e0e38173e53f5adc77e212bac6c3
DArkwa Bright <darkwadev@gmail.com>
2026-02-02 11:33:35 +0000

    dict methods in use

--- dictDataType.py
+++ dictDataType.py
@@ -5,4 +5,18 @@ my_dict = {
     "city": "New York"
 }
 print(my_dict)
+
+#access dictionary items
 print(my_dict["name"])
+
+#get all keys
+print(my_dict.keys())
+
+#get all values
+print(my_dict.values())
+
+#add new item to the dictionary
+my_dict["gender"] = "Female"
+print(" ")
+
+print(my_dict)
