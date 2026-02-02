ce4a95a9a63f84e191aacff4eeeb0e365829e1df
DArkwa Bright <darkwadev@gmail.com>
2026-02-02 22:09:48 +0000

    short hand if else added

---
+++ ifAndElse.py
@@ -0,0 +1,12 @@
+#short hand if 
+if 20 > 10: print("20 is greater than 10")
+print(" ")
+#short hand if else
+a= 15
+b=30
+print("a is greater") if a>b else print("b is greater ")
+
+#chaining consitional expression
+x= 350
+i=150
+print("x is lesser") if x<i else print("i is lesser") if i<x else print("they are equal")
