b7e917e63d32ece01d5c9affc30097100f4c20ee
DArkwa Bright <darkwadev@gmail.com>
2026-02-02 11:23:04 +0000

    set DAta type added

--- setDAtaType.py
+++ setDAtaType.py
@@ -1,3 +1,25 @@
 mySet = { "Bright", 24, True, 45.67 }
 print(mySet)
 print(len(mySet))
+print(" ")
+
+#access a set item 
+colors = { "Red", "Blue", "Green", "Yellow" }
+for x in colors:
+    print(x)
+print(" ")
+
+#add a whole new set to an existing set
+newColors = { "Brown", "Purple", "Orange" }
+colors.update(newColors)
+print(colors)
+print(" ")
+
+#add a single item to a set
+colors.add("Black")
+print(colors)
+print(" ")
+
+#remove an item from a set
+colors.remove("Green") # raises an error if the item to remove does not exist
+print(colors)   
